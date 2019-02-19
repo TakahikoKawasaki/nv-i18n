f36dc92e17d2d57178949c446240011031a0a6d6
Takahiko Kawasaki <taka@authlete.com>
2019-02-20 00:06:22 +0900

    Updated the year part of the copyright in CountryCode.

--- src/main/java/com/neovisionaries/i18n/CountryCode.java
+++ src/main/java/com/neovisionaries/i18n/CountryCode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2015 Neo Visionaries Inc.
+ * Copyright (C) 2012-2019 Neo Visionaries Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
